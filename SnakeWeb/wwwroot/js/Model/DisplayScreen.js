1a61325ad171b8075b0df9229e8c3abaf0cd978d
Anh Phương Phạm <phamtitans@gmail.com>
2023-01-20 19:19:50 +0700

    update main code js

---
+++ SnakeWeb/wwwroot/js/Model/DisplayScreen.js
@@ -0,0 +1,18 @@
+﻿
+export class Screen {
+    constructor(divId) {
+        this.sDiv = document.getElementById(divId);
+        this.canvas = document.createElement("canvas");
+        this._context = null;
+    }
+    get context() {
+        //prepare screen
+        if (this._context == null) {
+            this.canvas.width = this.sDiv.offsetWidth - $(this.sDiv).css('padding-left').replace('px', '') * 2;
+            this.canvas.height = this.sDiv.offsetWidth / 2;
+            this.sDiv.insertBefore(this.canvas, this.sDiv.childNodes[0]);
+        };
+        this._context = this.canvas.getContext("2d");
+        return this._context;
+    }
+}
