a665c9b600088935a24d81847dcbfb2736106207
Anh Phương Phạm <phamtitans@gmail.com>
2023-01-23 02:18:01 +0700

    update control object

--- SnakeWeb/wwwroot/js/Engine/Caculate/CaculateEngine.js
+++ SnakeWeb/wwwroot/js/Engine/Caculate/CaculateEngine.js
@@ -1,6 +1,62 @@
 ﻿
 // caculate
+
+// control object
+
+export function moveup(object, yUnit, rateUnit) {
+    object.accelerationY -= yUnit * rateUnit;
+}
+
+export function movedown(object, yUnit, rateUnit) {
+    object.accelerationY += yUnit * rateUnit;
+}
+
+export function moveleft(object, xUnit, rateUnit) {
+    object.accelerationX -= xUnit * rateUnit;
+}
+
+export function moveright(object,xUnit,rateUnit) {
+    object.accelerationX += xUnit * rateUnit;
+}
+
+export function stopMove(object) {
+    //object.accelerationX = object.accelerationX*(-1);
+    //object.accelerationY = object.accelerationY*(-1);
+    //object.speedX = 0;
+    //object.speedY = 0;
+
+}
+export function stopMoveX(object) {
+    //if (object.maxSpeedX != 0)
+    //object.accelerationX = object.accelerationX * (-1);
+    //object.maxSpeedX = 0;
+}
+// new position
 export function newHeadSnakePos(headSnake) {
+    
+    headSnake.speedX += headSnake.accelerationX;
+    headSnake.accelerationX = 0;
+    if (headSnake.maxSpeedX != null && headSnake.maxSpeedX < Math.abs(headSnake.speedX)) {
+        headSnake.speedX = headSnake.speedX >= 0 ? headSnake.maxSpeedX : -headSnake.maxSpeedX;
+    }
+    headSnake.speedY += headSnake.accelerationY;
+    headSnake.accelerationY = 0;
+    if (headSnake.maxSpeedY != null && headSnake.maxSpeedY < Math.abs(headSnake.speedY)) {
+        headSnake.speedY = headSnake.speedY >= 0 ? headSnake.maxSpeedY : -headSnake.maxSpeedY;
+    }
+
+    //console.log(headSnake.direction);
+    headSnake.blockList.forEach(function (value, index) {
+        //if (value.point.x <= 200) {
+        //    console.log(value.point.x);
+        //    console.log(value.point.y);
+        //}
+        value.point.x += headSnake.speedX;
+        value.point.y += headSnake.speedY;
+    });
+}
+
+export function newHeadSnakePosSpecial(headSnake) {
 
     if (headSnake.speedY >= 10) {
         //headSnake.accelerationY = -0.5;
--- SnakeWeb/wwwroot/js/Main.js
+++ SnakeWeb/wwwroot/js/Main.js
@@ -17,13 +17,16 @@ var block2 = new Block(x, y+20, color, height, width, weight);
 var blocks = [block, block2];
 var blockShowList = [];
 var rateDPS = 20;
+var xUnit = 10;
+var yUnit = 10;
+var rateUnit = 0.1;
 
 startGame();
 function startGame() {
     console.log("start game!!");
-    headSnake1 = new Component(blocks);
-    headSnake1.accelerationX = 0.015;
-    headSnake1.accelerationY = 5;
+    headSnake1 = new Component(blocks,5,5);
+    //headSnake1.accelerationX = 0.015;
+    //headSnake1.accelerationY = 5;
     blockShowList.push(...headSnake1.blockList);
     myGameScreen1 = new Screen("screenGDiv");
     setInterval(updateGameScreen, rateDPS);
@@ -34,4 +37,33 @@ function updateGameScreen() {
     CEngine.newHeadSnakePos(headSnake1);
     DEngine.updateScreen(myGameScreen1, blockShowList);
 }
+
+//control object
+
+document.addEventListener("keydown", key => {
+    switch (key.keyCode) {
+        case 37:
+            CEngine.stopMove(headSnake1);
+            headSnake1.maxSpeedX = 5;
+            CEngine.moveleft(headSnake1, xUnit, rateUnit);
+            break;
+        case 38:
+            CEngine.stopMove(headSnake1);
+            CEngine.moveup(headSnake1, yUnit, rateUnit);
+            break;
+        case 39:
+            CEngine.stopMove(headSnake1);
+            headSnake1.maxSpeedX = 5;
+            CEngine.moveright(headSnake1, xUnit, rateUnit);
+            break;
+        case 40:
+            CEngine.stopMove(headSnake1);
+            CEngine.movedown(headSnake1, yUnit, rateUnit);
+            break;
+        case 32:
+            CEngine.stopMoveX(headSnake1);
+            break;
+    }
+});
+
 
